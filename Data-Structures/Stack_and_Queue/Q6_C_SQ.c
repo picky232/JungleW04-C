2dfb1fe010a1dd80a772e2dc8af486f15574043a
picky232 <mistick0215@gmail.com>
2026-09-15 22:12:10 +0900

    complete Q6

--- Data-Structures/Stack_and_Queue/Q6_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q6_C_SQ.c
@@ -111,7 +111,7 @@ int main()
 
 void removeUntil(Stack *s, int value)
 {
-/* add your code here */
+	while(s->ll.size&&s->ll.head->item!=value)pop(s);
 }
 
 //////////////////////////////////////////////////////////////////////////////////
